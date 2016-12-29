f8f56250e0efd184460262c34891831955e5455a
Chico Camargo <achno@users.noreply.github.com>
2016-12-29 04:44:29 +0000

    Add files via upload
    Added title again

--- mapa_logistico.js
+++ mapa_logistico.js
@@ -3,8 +3,8 @@ var rSlider, xSlider;
 function setup() {
   // create canvas
   createCanvas(windowWidth, windowHeight);
-  fill(242, 230, 213);
   textFont("sans",30);
+  fill(242, 230, 213);
 
   var r0 = 600;
   var x0 = 100;
@@ -12,11 +12,11 @@ function setup() {
 
   // create sliders
   rSlider = createSlider(0, 800, r0);
-  rSlider.position(100, 40);
+  rSlider.position(100, 100);
   xSlider = createSlider(0, 1000, x0);
-  xSlider.position(100, 100);
+  xSlider.position(100, 160);
   tSlider = createSlider(0, 100, dt0);
-  tSlider.position(100, 160);
+  tSlider.position(100, 220);
 }
 
 function make_axes(x1,y1,x2,y2){
@@ -74,15 +74,19 @@ function draw() {
   var x0 = xSlider.value();
   var dt = tSlider.value();
   background(100);
+
   noStroke();
-  text("taxa de reprodução (r)",    245, 55);
-  text("população inicial (x)",     245, 115);
-  text("tempo entre gerações (dt)", 245, 175);
+  textFont("sans",40);
+  text("Mapa Logístico: x(t+\u0394t) = r \u00B7 x(t) \u00B7 [ 1 - x(t) ]", 100, 55);
+  textFont("sans",30);
+  text("taxa de reprodução (r)",    245, 115);
+  text("população inicial (x)",     245, 175);
+  text("tempo entre gerações (\u0394t)", 245, 235);
 
   r = r/200.0
   x0 = x0/1000.0
   dt = dt
 
-  logistic(r,x0,dt,50,205,width-100,height-100);
+  logistic(r,x0,dt,50,225,width-100,height-80);
 
 }
